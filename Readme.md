258230b04625489def5c6e8488cad66187f6aaa8
Tech Yoghurt <nikhilvaidyar1997@gmail.com>
2020-04-03 11:24:20 +0530

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# UI-Dashboard Task
+- A task to create a navigation system using react-native & redux
+
+## Design Patterns
+- Applying structured styling using flexbox and stylesheet
+- Creating a UI/UX flow for the dashboard
+
+## Technologies used
+- React-Native
+- Redux
+- React Navigation
+- Ant Design
+
+## Issues
+
+
+## Edge Cases
---
+++ components/AppNavigator.js
@@ -0,0 +1,12 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Home from './Home';
+import Friends from './Friends';
+
+const AppNavigator = createStackNavigator({
+    Home: { screen: Home },
+    Friends: { screen: Friends },
+});
+
+export default createAppContainer(AppNavigator);
